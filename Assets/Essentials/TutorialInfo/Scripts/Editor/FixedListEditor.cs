ae15e391bffdd6386fc617ee8a4e018dfc3a23d3
F1nndegamer <f1nndevgamer@gmail.com>
2025-06-13 21:52:13 +0200

    files

---
+++ Assets/Essentials/TutorialInfo/Scripts/Editor/FixedListEditor.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(AxisStringOrder))]
+public class AxisStringOrderEditor : Editor
+{
+    private ReorderableList reorderableList;
+
+    private void OnEnable()
+    {
+        SerializedProperty listProp = serializedObject.FindProperty("axes");
+
+        reorderableList = new ReorderableList(serializedObject, listProp, true, true, false, false);
+
+        reorderableList.drawHeaderCallback = rect =>
+        {
+            EditorGUI.LabelField(rect, "Axes (Reorderable)");
+        };
+
+        reorderableList.drawElementCallback = (rect, index, isActive, isFocused) =>
+        {
+            SerializedProperty element = listProp.GetArrayElementAtIndex(index);
+            rect.y += 2;
+            EditorGUI.PropertyField(rect, element, GUIContent.none);
+        };
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        reorderableList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
