6532b464a62548ef92a320ee0e18c05dceaf0934
F1nndegamer <f1nndevgamer@gmail.com>
2025-06-22 17:02:52 +0200

    Tutorial

---
+++ Assets/Scripts/Managers/UI/Tutorial.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Tutorial : MonoBehaviour
+{
+    private String[] Tutorials;
+    public TextMeshProUGUI tutorialText;
+    string keybind = "";
+    string MoveKey = "";
+    string ResetButton = "";
+    void Start()
+    {
+#if UNITY_ANDROID
+        MoveKey = " Swipe left/right/up/down ";
+        keybind = "Tap the screen ";
+        ResetButton = " the reset button ";
+#else
+        MoveKey = " Use WASD "; 
+        keybind = "Use Tab ";
+        ResetButton = " R ";
+#endif
+        Tutorials = new String[]
+        {
+            "Welcome to the game!" + MoveKey + "to move to the end.",
+            keybind + "to switch active players.",
+            "Press" + ResetButton + "to reset the level if you get stuck.",
+        };
+
+        for (int i = 0; i < Tutorials.Length; i++)
+        {
+            if (i == SceneManager.GetActiveScene().buildIndex - 1)
+            {
+                tutorialText.text = Tutorials[i];
+                break;
+
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
